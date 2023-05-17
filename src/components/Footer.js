bdefe9d9bb0da76335741dd872c58f4b5f5b9bf9
emauricio100 <emauricio@ucdavis.edu>
2023-05-17 15:48:28 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Footer() {
-
+  let navigate = useNavigate()
   // get the navigate function
 
   const goBack = () => {
     // imperatively redirect back
-    
+    navigate(-1)
   }
 
   const goForward = () => {
     // imperatively redirect forward
-    
+    navigate(1)
   }
 
   return (
